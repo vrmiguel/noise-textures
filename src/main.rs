b050aa342dadbba9d5da54d39adc8ac050768616
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-08-04 21:01:13 -0300

    Add wood-like texture generation

--- src/main.rs
+++ src/main.rs
@@ -70,7 +70,7 @@ pub fn marble() {
     const TURBULENCE_POWER: f64 = 8.0;
     const TURBULENCE_ZOOM: f64 = 16.0;
     
-    let mut noise: Noise<H, W> = Noise::new();
+    let noise: Noise<H, W> = Noise::new();
 
 
     'running: loop {
@@ -106,8 +106,61 @@ pub fn marble() {
     }
 }
 
+pub fn wood() {
+    let mut canvas = Canvas::instantiate(128*8, 128*8);
+    let mut event_pump = canvas
+        .event_pump()
+        .expect("failed to instantiate event pump");
+
+    const H: usize = 800;
+    const W: usize = 800;
+    const XY_PERIOD: f64 = 12.0;
+    const TURBULENCE_POWER: f64 = 0.1;
+    const TURBULENCE_ZOOM: f64 = 128.0;
+    
+    let noise: Noise<H, W> = Noise::new();
+
+
+    'running: loop {
+        canvas.clear(Color::BLACK);
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+
+        for i in 0..128*8 {
+            for j in 0..128*8 {
+
+                let x_val = (i as f64 - W as f64 / 2.0) / (W as f64);
+                let y_val = (j as f64 - H as f64 / 2.0) / (H as f64);
+                let dist = (x_val.powi(2) + y_val.powi(2)).sqrt() +
+                    TURBULENCE_POWER * noise.turbulence(i, j, TURBULENCE_ZOOM) as f64 / 256.0;
+
+                let sine_val = 128.0 * (2.0 * XY_PERIOD as f64 * dist * PI).sin().abs();
+
+                let r = 80 + (sine_val as u8);
+                let g = 30 + (sine_val as u8);
+                let b = 30;
+
+                canvas.set_draw_color(Color::RGB(r, g, b));
+                let _ = canvas.draw_point(i, j);
+            }
+        }
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
+}
+
 pub fn main() {
     // random_noise()
     // cloud()
-    marble()
+    // marble()
+    wood()
 }
